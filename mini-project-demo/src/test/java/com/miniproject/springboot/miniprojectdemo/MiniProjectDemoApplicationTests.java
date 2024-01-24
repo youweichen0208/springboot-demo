eb19b39aa48bdab047c4b48759792467fe5e1ea5
Youwei Chen <youweichen0208@gmail.com>
2024-01-23 20:38:04 -0500

    mini project start

---
+++ mini-project-demo/src/test/java/com/miniproject/springboot/miniprojectdemo/MiniProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject.springboot.miniprojectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
